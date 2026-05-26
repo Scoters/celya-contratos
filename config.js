95f82145162954fb565e19c5256d43fcd6a66077
scoters <malakiandaron@hotmail.com>
2026-05-26 12:39:59 -0600

    Auto llenar campo de cuota

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzJ8KPOJYkWqqw1dJgbIfJ4onflFeqessvkzmk1d3PfXwzR5CEsxnMNfE6SVVfvCYWa/exec",
+  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbypT6LqzlW0Eq4cmVL2V7kx0yW3pJDjFJag3fedOr6vdaOFdMnJoHfDyrgrY1uwBEZE/exec",
   API_TOKEN: "celya_k9x2m_s7q4p"
 };
