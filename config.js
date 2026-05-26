5eddb28acac803ebb3d7c3b4c203592527c8b954
scoters <malakiandaron@hotmail.com>
2026-05-26 12:28:08 -0600

    New Url

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyS4luppbNTkW-YdC2XaRoCDkQhEOzentM7BRBnEhjo90Io44WIeUpxmgHsBiCmflg/exec",
+  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzJ8KPOJYkWqqw1dJgbIfJ4onflFeqessvkzmk1d3PfXwzR5CEsxnMNfE6SVVfvCYWa/exec",
   API_TOKEN: "celya_k9x2m_s7q4p"
 };
