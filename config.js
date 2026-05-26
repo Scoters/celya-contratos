2289f48af787a3b6dcf58cf371859a02354f193b
scoters <malakiandaron@hotmail.com>
2026-05-25 20:24:53 -0600

    Añadido panel dashboard para generar enlaces automáticamente

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxDGLA4-kO4PLu5tr5J1LkSky46AiVEGyufHniDFWAZUBoBnmGgt73d0FJfLCJ0MVpB/exec"
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxe3_YJ3MhdTqj6c4XoR3wde4y5Ygwih9AVlCg2nochY8eKPrva82WeN-l3APsczDbE/exec"
 };
