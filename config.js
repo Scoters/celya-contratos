d6aa1b7e085b27f368203bcc95608cc45c62d825
scoters <malakiandaron@hotmail.com>
2026-05-25 20:47:15 -0600

    Dashboard v2 con integración total de Inventario y selección de plazos

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxe3_YJ3MhdTqj6c4XoR3wde4y5Ygwih9AVlCg2nochY8eKPrva82WeN-l3APsczDbE/exec"
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwaXTMikGg3PM1EWvCAQtsemggJhevivOCn5e8DuxbH-eD4Adldt3EBLBgi0WBRuESO/exec"
 };
