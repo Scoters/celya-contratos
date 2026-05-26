685697c6dccdf60d8ebe888a47fa14ff76283a89
scoters <malakiandaron@hotmail.com>
2026-05-25 20:02:21 -0600

    Versión unificada y definitiva con diagnóstico de fotos

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxPe7MuWM_lk7OHdSbPJfZYqBbRWjC3Rnn675TV6jgofYa8sV5xB1ULiof0Y2kHL9dl/exec"
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzi1E-WMK0Ub7eXP-xDnaHqWGORVuQ_vh0enlyrcXhx6gIzF1BmEzOKctymrX_xT9iv/exec"
 };
