861e9b8c28cb887a96076875ffb4a81faf3f941d
scoters <malakiandaron@hotmail.com>
2026-05-26 00:22:20 -0600

    Got new token

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzopDp1_iK5Fn_JNBAwYN8hFBVyP4eURtOXNpuRALZhXyOxDWKMGlhAvSclnt95I-Wq/exec"
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyXNbXgwdQgRKT6NRU81ynh8aip2r_DYix8rLwhpPpBzZwSv4nZTDRWZN2xvI0OjnI/exec"
 };
