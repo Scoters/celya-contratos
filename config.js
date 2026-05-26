336f251b89af0591ac3d03f7be509377b727de4d
scoters <malakiandaron@hotmail.com>
2026-05-26 01:55:02 -0600

    Changed script

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyHdJGJdDvFRHb0dq_YWu6BUVaHfSjZmYIdjtATIsfdVmCVL3pfdLM7T6ucs_cb4_gn/exec"
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxz2ZDKUiCHhejVlBWjU3f8deaQ8cIePXAvmupZR7liJcx-89BDXUdlLfB6HqqbL1DB/exec"
 };
