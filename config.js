8ff3526082da8103f58147b84271d83f4c5f3c66
scoters <malakiandaron@hotmail.com>
2026-05-26 12:22:56 -0600

    New Security Features

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzUceXSTSSnFuUroNI4z6EJdZ2jj1pXP4P-eMrmpe5d0l-DZXaq-30zeNdkTzpxM125/exec"
+  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyS4luppbNTkW-YdC2XaRoCDkQhEOzentM7BRBnEhjo90Io44WIeUpxmgHsBiCmflg/exec",
+  API_TOKEN: "celya_k9x2m_s7q4p"
 };
