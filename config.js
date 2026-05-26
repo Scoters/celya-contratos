a14e8eee246a0b6600c148d5d841c18c6f136cfb
scoters <malakiandaron@hotmail.com>
2026-05-26 03:41:23 -0600

    Fixed Client list

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxJHf04vv5MLFhr-ObkJ0uc4LWpmO6igiUYTbtQUhLeNnDL7reuATpYRvxeQETr0C0S/exec"
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzPPByf5C2a15pT8ulTQDXAa2C7WvgTmEI1uYt9iN7YifPP9j73TS3WAau2kwnWfDze/exec"
 };
