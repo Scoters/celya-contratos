29841054c651596acdc7bab768fafe95cd4d8280
scoters <malakiandaron@hotmail.com>
2026-05-26 02:24:12 -0600

    New Code

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbysDKzihG9eCIzp7Y5geEp3o0lSjL7M8iwrnTCMsgZRswvCxhsoNXKm0wB875gj3DaO/exec"
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxETgs59qGGgZt2jsxGy5tL5oM1FTxmfbt-dzxTmDJuSOIo3gTT1N1ULhDqYWd6vMmq/exec"
 };
