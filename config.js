8e84b7a510e4d65c72a75c637f5a2d321656bc50
scoters <malakiandaron@hotmail.com>
2026-05-25 20:15:14 -0600

    Automatización total de parámetros ocultos para Autocrat

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzi1E-WMK0Ub7eXP-xDnaHqWGORVuQ_vh0enlyrcXhx6gIzF1BmEzOKctymrX_xT9iv/exec"
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxDGLA4-kO4PLu5tr5J1LkSky46AiVEGyufHniDFWAZUBoBnmGgt73d0FJfLCJ0MVpB/exec"
 };
