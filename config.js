697363c6e5bb70ef3bcf59db27725bb6692ef653
scoters <malakiandaron@hotmail.com>
2026-05-28 13:42:01 -0600

    Agrega frecuencia quincenal, cálculo de cuota dinámico y columna día de pago

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwQMiT26gO-gmAN__gVpZKIq623NjmFLf_bLLvPyt8gseskfz4RcRNI_gX0_ZvnW9ta/exec",
+  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbw7u6z6fpaQz3FSHVVkb0OgE0TuCiT-f_pqmWxBj7n41Uab59paAhG3_pcbei2VJ6TE/exec",
   API_TOKEN: "celya_k9x2m_s7q4p"
 };
