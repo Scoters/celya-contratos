7cb6a8e8172c78892d5b5526d4313030e1910b27
scoters <malakiandaron@hotmail.com>
2026-05-28 14:55:12 -0600

    Renombra campo Monto Inicial a Precio de Contado en el formulario

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxhazBxX0rzrrA5nlEaKyPaj4uE8oCP4Ny8bnzKAGCJMSnE36Z_86vlI1ND7huXru7D/exec",
+  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyNoVhAI0-CkNcbk7gojjK8boc6YqySPXOzFctkmXfm1Fn4aaw3VCR1VISvfabhiBjo/exec",
   API_TOKEN: "celya_k9x2m_s7q4p"
 };
