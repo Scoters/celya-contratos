2cccc897d3c6e228bcae9796bf296e4cbac11515
scoters <malakiandaron@hotmail.com>
2026-05-28 15:55:59 -0600

    Cobranza dinamica

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzela7AKeSnxnF2bUJVd-HxI8vE8aZ0usQE-ZpepOsR5TPM1OB3-Cl8JaZeko8vZPqY/exec",
+  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzNyBPc5nFkxIkKid3VJPU7DczI03HRyCmM216ZMNFUiGb9o3vzo4oCLCU8hZZMFqy5/exec",
   API_TOKEN: "celya_k9x2m_s7q4p"
 };
