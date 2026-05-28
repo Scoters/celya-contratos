1c37f2378e658abfefd559145779ef2e6337fbbd
scoters <malakiandaron@hotmail.com>
2026-05-28 11:52:55 -0600

    added login page

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxO6AW1tk19H6_BVlaNcEyB-na6Z3cHFtIbk64_T35Ran1BJWj_gDEVnhjIDz3enP7U/exec",
+  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxRV5jnSsAI7fRqde_yNoYsLYtJGytri_7pxBrHcKqLV0Y0rv4GnYchKPBSg2flor-Y/exec",
   API_TOKEN: "celya_k9x2m_s7q4p"
 };
