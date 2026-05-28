211cb0ccee9ff0c0cd6cd2d9b21e2e25a8fb90ec
scoters <malakiandaron@hotmail.com>
2026-05-28 14:00:08 -0600

    Fusiona frecuencia y día de pago en selector único para optimizar Columna S para el bot

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbytjQylUNnhCUsKBMEnMQ1-leCcstfMTLyrUGS6lxsbQdcr44lOZ6CYYp76Y7vz7cXN/exec",
+  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwjtNbk7xPytIIHoBjFsVl3t5zbAzs3C8yyCCnFiExEbK13MC0-G4LbFoKPxsLCx7GU/exec",
   API_TOKEN: "celya_k9x2m_s7q4p"
 };
