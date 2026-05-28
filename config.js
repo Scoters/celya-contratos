343e1f935850d422506c21b9bfaacb0d87338b0b
scoters <malakiandaron@hotmail.com>
2026-05-28 13:32:48 -0600

    Cambie cuotas

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbz62U-6roRwHRYhTgux6Dk4elW5NnBcjjsCK8Z81StbO19znKrp9Hes4ReddqpDaMgr/exec",
+  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwQMiT26gO-gmAN__gVpZKIq623NjmFLf_bLLvPyt8gseskfz4RcRNI_gX0_ZvnW9ta/exec",
   API_TOKEN: "celya_k9x2m_s7q4p"
 };
