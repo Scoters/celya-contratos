a9484c866ea63f69dfc406d80a1b304267b41de2
scoters <malakiandaron@hotmail.com>
2026-06-01 01:58:01 -0600

    Changed url

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwmWJ3LEiH82XpgIsRp2xhjKi6eR56Y3xsagwWggtdt54cZFdcqQN0aE40T8twp-IRl/exec",
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyr7hbYLs_Crt_0cV4lbvfM-EgkGfH8EcmDqFTgxCDnWZtj5JAGddyKj4YfQGP36_fN/exec",
     API_TOKEN: "celya_k9x2m_s7q4p"
 };
