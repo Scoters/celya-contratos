fbb9f35bc84eba14d1c8bd67a71e9d5de68a64b9
scoters <malakiandaron@hotmail.com>
2026-06-01 00:19:59 -0600

    Buscador de celulares

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyd6vpdBO0PdAzpQ56l8a8JvMcmdG7nsVcC5iInJd3wQCfliuHPPU13y_C2-6fjgQYD/exec",
-    API_TOKEN: "celya_k9x2m_s7q4p",
-    LOGO_URL: "https://drive.google.com/uc?export=view&id=13bnQONkSe9Sd1zsC0Iw8LoPmx1ljvOtQ"
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxIlvCHmJdFReVZ1il5HBwN-fLc_N2jRK8CkJsMtc-Meh_mPVNU3PrQ4FROLUDyooOd/exec",
+    API_TOKEN: "celya_k9x2m_s7q4p"
 };
