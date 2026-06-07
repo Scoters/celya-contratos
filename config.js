239d89cd986ad3e01be496ff342d92656f8bc7e3
scoters <malakiandaron@hotmail.com>
2026-06-07 02:07:13 -0600

    Cambios al catalogo

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbw232SIc7LE3vm4Cr3NqOO2bVJCAy5nux-1D97j2rkPASiaVxP1O7tcMXBzEo0AjQI/exec",
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzvypq7VIRL6ZLgwUEjT83oY8D7xRJU5eJNv1syW9oh1wb6-AwoQw3_5wnnAvclloQA/exec",
     API_TOKEN: "celya_k9x2m_s7q4p",
     PRELOADED_CATALOG: []
 };
