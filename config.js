598275a233e372cf31de9af77cd04c71a7a6aa41
scoters <malakiandaron@hotmail.com>
2026-06-07 19:24:35 -0600

    Cambios al catalogo, formulario

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyJv0Q4T7qR0uzbjpgZ7OBuOxcPm8DCALGFU7KnFfWJpsuMxPAlZo2b-2Tz3R-hPfYn/exec",
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycby8lTh35xJe0r002Zq_DlIkAhCR1PPqUJy3YWkf4Bwq1DL47XJgfyNjXOdFcXReR6SE/exec",
     API_TOKEN: "celya_k9x2m_s7q4p",
     PRELOADED_CATALOG: []
 };
