ddcc574fbce7fe63b2c65ebc6c2374039e0d561e
scoters <malakiandaron@hotmail.com>
2026-06-13 19:43:07 -0600

    Update: Configuración de la nueva URL del backend de Google Apps Script

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxsCmXV16iTAgq2OYl7qhQZyAYvYOkRmYnjd-ZIylXDGozzYjPlvZP4Z9dJF1moiW2j/exec",
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbz1Yo1lowDIa-NnPJo9Etrv7ZhW6fZyVuBCGDVO9IKVl8Nm1_y1ZhEhgaMs5RqjmKrf/exec",
     API_TOKEN: "celya_k9x2m_s7q4p",
     PRELOADED_CATALOG: []
 };
