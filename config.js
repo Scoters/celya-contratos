851c37857f1c5c2337c0bf5941e4f5d501b587d0
scoters <malakiandaron@hotmail.com>
2026-06-14 03:37:27 -0600

    Update Google Apps Script URL in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbz1Yo1lowDIa-NnPJo9Etrv7ZhW6fZyVuBCGDVO9IKVl8Nm1_y1ZhEhgaMs5RqjmKrf/exec",
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwEU_LxThEC2_MLNHNMCl499vQXx4XwuUzUsraq3Q1cgtGv63NPl2ROWr-R7QURL1wn/exec",
     API_TOKEN: "celya_k9x2m_s7q4p",
     PRELOADED_CATALOG: []
 };
