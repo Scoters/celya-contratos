5a59ced37f4e2e7ae0d7d4181da7c60560a51e1c
scoters <malakiandaron@hotmail.com>
2026-06-15 00:11:54 -0600

    Update script URL, placeholder price conversion fix, and support for iPhone validation

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyQ4jG-e9WLMoklZfSBvcBW6DMSarwvnYiu37qqgwELUSBVbXLMYKvPD8TG-CRTx9fv/exec",
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzkhhyTdhNpOgJuAUG0C3WGaypk1dPW6_jeGCoSdwvwZZKUroVwJJ1JZrIEwUbhdjqZ/exec",
     API_TOKEN: "celya_k9x2m_s7q4p",
     PRELOADED_CATALOG: []
 };
