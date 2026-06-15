fcf99190f11761f81e05e4db44bcf4d640fdffe9
scoters <malakiandaron@hotmail.com>
2026-06-14 22:09:02 -0600

    Actualizar GOOGLE_SCRIPT_URL en config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbx6o7UyLx09pABMBlArPs0IK5D06TsXtdn-3gLammbgpsuQ4VMzd_TX--dbIRRKOdvF/exec",
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyQ4jG-e9WLMoklZfSBvcBW6DMSarwvnYiu37qqgwELUSBVbXLMYKvPD8TG-CRTx9fv/exec",
     API_TOKEN: "celya_k9x2m_s7q4p",
     PRELOADED_CATALOG: []
 };
