f052f6dae64d9c03c4d6f50bb0a83a9bb538dcc3
scoters <malakiandaron@hotmail.com>
2026-06-14 20:06:50 -0600

    Fix placeholder tool: photo/price display and add to inventory button

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycby2nHvkvQEl5qT0e3nTO2jGrFROuXEkj0ZhM-xwMNmFZ5Q545SHEkzEvgBhx8vBkzXI/exec",
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxVDFudYo9S1A3PYiVyqHID2bolfwS6goXKsU1ckfdnCRK38xECRjxp6YVB2lUdWJ5Y/exec",
     API_TOKEN: "celya_k9x2m_s7q4p",
     PRELOADED_CATALOG: []
 };
