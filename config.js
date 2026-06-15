24ca5534b2a52dd6e3b3b5fda30f90718f14f799
scoters <malakiandaron@hotmail.com>
2026-06-14 18:47:10 -0600

    chore: update GOOGLE_SCRIPT_URL to latest deployment

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxRVvekS1xS0XjUnzulMI2Vz7GzXAvf-Up8-Qq0T3XnZaz95FySR6sYPpQfswK0oE_L/exec",
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycby2nHvkvQEl5qT0e3nTO2jGrFROuXEkj0ZhM-xwMNmFZ5Q545SHEkzEvgBhx8vBkzXI/exec",
     API_TOKEN: "celya_k9x2m_s7q4p",
     PRELOADED_CATALOG: []
 };
