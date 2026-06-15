d9b0df7c39d46cec499776f0ab88dcfb6d9cf274
scoters <malakiandaron@hotmail.com>
2026-06-14 20:23:09 -0600

    Allow 'siendo pagado' devices in index dropdown if contract is not registered yet

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxVDFudYo9S1A3PYiVyqHID2bolfwS6goXKsU1ckfdnCRK38xECRjxp6YVB2lUdWJ5Y/exec",
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbx6o7UyLx09pABMBlArPs0IK5D06TsXtdn-3gLammbgpsuQ4VMzd_TX--dbIRRKOdvF/exec",
     API_TOKEN: "celya_k9x2m_s7q4p",
     PRELOADED_CATALOG: []
 };
