b58532bd8bf689b0c448218de362301dd1f96942
scoters <malakiandaron@hotmail.com>
2026-06-15 02:07:27 -0600

    feat: Add import badge, update PDP alert, and set new Google Script URL

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzmDlbaC5W9OYtVoTS60jOM0BOLOcWuIwKYK_aR6jBxEnOKt3-39kvkiCEQ4MmvbHWp/exec",
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzjZt--A2pdE4vuhOUVCskRWLfVNqGkRkshvJ6AE672-RWCAWe11hOPRemvzvL-0eA1/exec",
     API_TOKEN: "celya_k9x2m_s7q4p",
     PRELOADED_CATALOG: []
 };
