c1863fdedae802a98ea29c58886494aaa2a3da58
scoters <malakiandaron@hotmail.com>
2026-06-15 01:57:20 -0600

    chore: update GOOGLE_SCRIPT_URL to new deployed Web App

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxsH9jd1JlcNRCLLjES49Uj8WeSbxvPSdinv2lz4bnVvt0QUD9vrlNsEV6WX4iPcMPp/exec",
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzmDlbaC5W9OYtVoTS60jOM0BOLOcWuIwKYK_aR6jBxEnOKt3-39kvkiCEQ4MmvbHWp/exec",
     API_TOKEN: "celya_k9x2m_s7q4p",
     PRELOADED_CATALOG: []
 };
