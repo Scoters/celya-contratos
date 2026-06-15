7de71c608a0f14f11f802f212498016119b07285
scoters <malakiandaron@hotmail.com>
2026-06-14 18:36:50 -0600

    feat: Telegram price breakdown + new script URL
    - script.gs: add price breakdown block (costo ML, ganancia %, precio
      contado, enganche) to Telegram notifications on new credit/cash quotes
    - config.js: update GOOGLE_SCRIPT_URL to latest deployment

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwgPAL2xSKEpqnIZNFu2GviCqjLnpUWvlnluVOQC6q6o8boPd9YtmOFexnZnEtITwQZ/exec",
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxRVvekS1xS0XjUnzulMI2Vz7GzXAvf-Up8-Qq0T3XnZaz95FySR6sYPpQfswK0oE_L/exec",
     API_TOKEN: "celya_k9x2m_s7q4p",
     PRELOADED_CATALOG: []
 };
