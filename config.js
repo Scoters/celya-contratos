29744898d755f123481e21a2180b3407b3b73b9a
scoters <malakiandaron@hotmail.com>
2026-06-19 17:39:22 -0600

    Fix Column Q, align down payments, clean Siendo Pagado casing, and update script URL

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyFrurUgV8E4peD5AJkg1u57-caVhTE5UyuiTk-SdsgCJWIKgiKji31ujC3gCWqMHrl/exec",
-    API_TOKEN: "celya_k9x2m_s7q4p",
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbw9S3znqXJgysnb-EjfmjV8aCUn5OID1InsJuYkEJzoGVHN2B6ujE-TUocEQ5bQCV4f/exec",
+    API_TOKEN: "celya_548d5128f1d6",
     PRELOADED_CATALOG: []
 };
