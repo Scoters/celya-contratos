ad9942c61f70c31da2decf0ad2ccae98610ccc52
scoters <malakiandaron@hotmail.com>
2026-06-19 19:59:12 -0600

    Actualizar URL de despliegue de Apps Script para webhook corregido

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbymokeXA7CbJYGo0wMjx09ug3C5FonwTa7z6qcneivvTUr6dYW_2_-wVBAUAUKUIyYx/exec",
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwCe3ilAsENOJ-vGqGBOvALwaa5mVGA2SQHcTv6IwVgXfMywf0YIoc0xuoJ2kZxL6ol/exec",
     API_TOKEN: "celya_548d5128f1d6",
     PRELOADED_CATALOG: []
 };
