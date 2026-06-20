8c5abad8592f254005b9e4bafcb75d9afeefa460
scoters <malakiandaron@hotmail.com>
2026-06-19 19:33:06 -0600

    Actualizar URL de despliegue de Apps Script

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbz5VahgijREfJBjPC5kY9MNV8zo08iCAX-Jb-JSp8PJXzpvcrpkGQ29vzyetcLK_HZb/exec",
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzN5RjyW-0wUq_1LdxZNhRrWgO2w2jzVIxUT4wVSyzYmLcjkmZc5cS062orTamWfluN/exec",
     API_TOKEN: "celya_548d5128f1d6",
     PRELOADED_CATALOG: []
 };
