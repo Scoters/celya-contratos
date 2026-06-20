1de06151fb8f56224816124bdbafc49e899047fb
scoters <malakiandaron@hotmail.com>
2026-06-19 18:04:46 -0600

    Update Google Script deployment URL to latest version

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbw9S3znqXJgysnb-EjfmjV8aCUn5OID1InsJuYkEJzoGVHN2B6ujE-TUocEQ5bQCV4f/exec",
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbywJtXOH7h3x7ulYVTYOVTE9IjvUcQEhAQm6VHfXTQIbARHAh8-e5Di3XP274wifj72/exec",
     API_TOKEN: "celya_548d5128f1d6",
     PRELOADED_CATALOG: []
 };
