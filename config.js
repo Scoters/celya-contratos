3ff9cff1bc2785bf698bceb3a1bf48469503664e
scoters <malakiandaron@hotmail.com>
2026-06-19 19:37:38 -0600

    Actualizar URL de despliegue - fix función Telegram unificada

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzN5RjyW-0wUq_1LdxZNhRrWgO2w2jzVIxUT4wVSyzYmLcjkmZc5cS062orTamWfluN/exec",
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzOT6t0vJOwJJU3q-xB4mkU9f568UU27QxZX3NcZ_NMd0Xpz3EFnUcMm-FudfQxQK99/exec",
     API_TOKEN: "celya_548d5128f1d6",
     PRELOADED_CATALOG: []
 };
