da4cfed2cf1b3b0b6a39273f1986786b6c31c34a
scoters <malakiandaron@hotmail.com>
2026-06-19 20:01:38 -0600

    Actualizar URL de despliegue de Apps Script para limpiar actualizaciones pendientes en Telegram

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwCe3ilAsENOJ-vGqGBOvALwaa5mVGA2SQHcTv6IwVgXfMywf0YIoc0xuoJ2kZxL6ol/exec",
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwuAlcTG9Rjh886OzhDCn6RXbY9B0nqfcFRbuqStZF6bJuodkhcqyWBtFtjLzzE30nN/exec",
     API_TOKEN: "celya_548d5128f1d6",
     PRELOADED_CATALOG: []
 };
