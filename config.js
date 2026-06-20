0505b5ce0bc77d14b432409a42413e4af663d342
scoters <malakiandaron@hotmail.com>
2026-06-19 19:13:51 -0600

    feat: simplify morning telegram summary, add payment confirmation receipt copy modal, and integrate Telegram Bot Webhook commands

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbywJtXOH7h3x7ulYVTYOVTE9IjvUcQEhAQm6VHfXTQIbARHAh8-e5Di3XP274wifj72/exec",
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbz5VahgijREfJBjPC5kY9MNV8zo08iCAX-Jb-JSp8PJXzpvcrpkGQ29vzyetcLK_HZb/exec",
     API_TOKEN: "celya_548d5128f1d6",
     PRELOADED_CATALOG: []
 };
