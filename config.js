c67f8c12cc00303ed5ce5080d003becb08c9dd1e
scoters <malakiandaron@hotmail.com>
2026-06-19 19:46:00 -0600

    Actualizar URL de despliegue de Apps Script a versión con mejor manejo de errores

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzOT6t0vJOwJJU3q-xB4mkU9f568UU27QxZX3NcZ_NMd0Xpz3EFnUcMm-FudfQxQK99/exec",
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbymokeXA7CbJYGo0wMjx09ug3C5FonwTa7z6qcneivvTUr6dYW_2_-wVBAUAUKUIyYx/exec",
     API_TOKEN: "celya_548d5128f1d6",
     PRELOADED_CATALOG: []
 };
