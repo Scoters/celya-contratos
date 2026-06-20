e695a9ac8e1b3f0d8d53057f6a4f2c506395ab50
scoters <malakiandaron@hotmail.com>
2026-06-19 20:05:11 -0600

    Actualizar URL de despliegue de Apps Script para diagnósticos de teclado

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwuAlcTG9Rjh886OzhDCn6RXbY9B0nqfcFRbuqStZF6bJuodkhcqyWBtFtjLzzE30nN/exec",
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbx0XfBZUV6ZRANJPTQuLuV9FcvdUeLxh82F5_ieYR2mQxP85lY8Mr7Re79o_SFPBfyA/exec",
     API_TOKEN: "celya_548d5128f1d6",
     PRELOADED_CATALOG: []
 };
