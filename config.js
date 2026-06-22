566ae9f85b5f37a251fc357423d74e3c19674317
scoters <malakiandaron@hotmail.com>
2026-06-22 13:13:27 -0600

    Fix syntax errors, update Google Script URL, and set comfortable plan (Plan Cómodo) as default down payment

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxTleJb9lm7hT780G61keszZtvuimKuH9wdpjYvL12NuoJcg4dtigftmvztTWyHFNht/exec",
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbykztbMsCbmeo5C0b1mbLQGO_UTPWzVhbTbfE43jX0yvw5MrCUvhQSSArTcI_YoSxC/exec",
     API_TOKEN: "celya_548d5128f1d6",
     PRELOADED_CATALOG: []
 };
