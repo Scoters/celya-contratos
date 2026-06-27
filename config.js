89e31a6d723ad1cbc848abaea6bd636255f176d4
scoters <malakiandaron@hotmail.com>
2026-06-27 17:42:08 -0600

    Security fixes, PDF download button in portal, phone length restriction, and ignore local documents

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzzkEbeOq7HIJ9KMAhArO567jFGPcz5V-l9Bdqc_ykcwYBVmesIQhypb_Tusl4af-kD/exec",
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbx_DHa9zNyGFZrfr6dS4NgoW9xbArfpskejYi_UP_o3NDP6ovIx7m_Aj_KIp0DGNAHJ/exec",
     API_TOKEN: "celya_548d5128f1d6",
     PRELOADED_CATALOG: []
 };
