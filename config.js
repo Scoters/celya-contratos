86957386cf8c6e97ebdef0ec938cf1635865deb2
scoters <malakiandaron@hotmail.com>
2026-06-27 17:46:39 -0600

    Ignore config.js and .env, dynamically load env configurations

--- config.js
+++
@@ -1,5 +0,0 @@
-const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbx_DHa9zNyGFZrfr6dS4NgoW9xbArfpskejYi_UP_o3NDP6ovIx7m_Aj_KIp0DGNAHJ/exec",
-    API_TOKEN: "celya_548d5128f1d6",
-    PRELOADED_CATALOG: []
-};
