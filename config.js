0ae96fa5f19c35a466e5468924a60fa50adab73c
scoters <malakiandaron@hotmail.com>
2026-06-26 19:28:01 -0600

    Actualizar URL de Web App y configuraciones

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbykztbMsCbmeo5C0b1mbLQGO_UTPWzVhbTbfE43jX0yvw5MrCUvhQSSArTcI_YoSxC/exec",
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzzkEbeOq7HIJ9KMAhArO567jFGPcz5V-l9Bdqc_ykcwYBVmesIQhypb_Tusl4af-kD/exec",
     API_TOKEN: "celya_548d5128f1d6",
     PRELOADED_CATALOG: []
 };
